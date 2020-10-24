88dfd3c949901ecad20793633f8e13d3877966d5
blaze-github <blaze-github@users.noreply.github.com>
2020-10-25 00:23:44 +0200

    Updatet comments and added console message on loggin

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Bot Documentation
 This is my first bot published on github
+
+
+# Functions
+### 1. Timer
--- bot/__main__.py
+++ bot/__main__.py
@@ -1,9 +1,15 @@
-import discord
-from discord.ext import commands
+import discord                                                                  #  Import discord library
+from discord.ext import commands                                                #  From discord library import commands
 
 
 bot = discor.Client()
 bot = commands.Bot(command_prefix=config.PREFIX)                                #  Import bot prefix from config file
+
+@bot.event
+async def on_ready():
+    print('Logged in as')                                                       #  If bot is ready print this message in console
+    print(bot.user.name)                                                        #  Print in console bot name
+    print(bot.user.id)                                                          #  Print in console bot id
 
 @bot.event
 async def on_message(message):
@@ -14,12 +20,12 @@ async def on_message(message):
 
 @bot.command()
 async def timer(ctx):
-    count = 320                                                                 # Time in seconds
+    count = 320                                                                 #  Time in seconds
     countdown = await ctx.message.channel.send('**{}**'.format(count))          #  Send first message to discord channel
-    for i in range(count, -1, -1):                                              #  For loop
+    for i in range(count, -1, -1):
         await asyncio.sleep(1)                                                  #  Wait 1 second
         await countdown.edit(content='**{}**'.format(i))                        #  Change message content
-        if i == 0:                                                              #  If i = 0 someting
+        if i == 0:
             await countdown.edit(content='**Time out**')                        #  Change message content
 
 
